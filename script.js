9fc1884b64b7cdbf87f44016a53311ae11d7c584
Salvador Guzman Jr <guzmansalv@gmail.com>
2020-04-11 03:15:35 -0700

    add everything

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const doc = document;
 const tab1 = doc.getElementById('tab1body')
 const tab2 = doc.getElementById('tab2body')
 
-function toggleTabs() {
-    const temp = tab1.style.display;
-    tab1.style.display = tab2.style.display;
-    tab2.style.display = temp;
+function displayTab1() {
+    tab1.style.display = 'inline-block';
+    tab2.style.display = 'none';
+}
+
+function displayTab2() {
+    tab2.style.display = 'inline-block';
+    tab1.style.display = 'none';
 }
